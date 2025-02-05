430753ba794fd8ddf57075eff9dc7d4d075ecafe
Andres Flores <andres.floresr@ieee.org>
2025-02-04 22:05:36 -0600

    feat: add example data and datasets module

--- src/pycounts_co/__init__.py
+++ src/pycounts_co/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_co")
+
+# populatepackagenamespace
+from pycounts_co.pycounts_co import count_words
+from pycounts_co.plotting import plot_words
